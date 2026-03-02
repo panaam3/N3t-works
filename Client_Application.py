803d143667a0a21b89e00ebd17c20d3571666db3
Nsika18 <kungawomase4@gmail.com>
2026-03-02 16:43:38 +0200

    Implement TCP connection and command message structure

--- Client_Application.py
+++ Client_Application.py
@@ -1,11 +1,43 @@
 # client 
+from socket import *
+import json
+import datetime
+
 
 class client:
     def __init__(self):
+        #self.server_ip = server_ip
+        #self.server_port = server_port
+        #self.username = username
+        #self.ip_addr = ip_addr
         pass
+
+    def tcp_connect(self, server_ip, server_port):
+        # establish TCP connection with the server
+        self.server_ip = server_ip
+        self.server_port = server_port
+
+        client_socket = socket(AF_INET, SOCK_STREAM) 
+        client_socket.connect((server_ip,server_port)) # establish the TCP Connection 
+        #self.client_socket = client_socket
+
     def send_command(self, command, body):
         # send command message to the server
-        pass
+        header  = {
+            "msgType": "COMMAND",
+            "command": command,
+            "version": "MMMP/1.0",
+            "seqNo": 2001,
+            "senderId": self.username,
+            "timestamp": datetime.datetime.now().isoformat(),
+            "bodyLength": len(json.dumps(body))
+             }
+        
+        message = {
+            "header": header,
+            "body": body
+        }
+        return message
     
     def send_data(self, command, body):
         # send data message to the server
@@ -13,11 +45,9 @@ class client:
 
     def receive_message(self, message):
         # receive message from the server and process it
-        #Still need to disect the message to get to the request
+        #Still need to disect the message to get the request
         pass
 
-
-    
 
 """
 
