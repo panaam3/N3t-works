aad3b7c3538232cc874bb9013361d06a69a4aeb7
Nsika18 <kungawomase4@gmail.com>
2026-03-02 17:29:10 +0200

    more implementation

--- Client_Application.py
+++ Client_Application.py
@@ -1,25 +1,23 @@
 # client 
 from socket import *
-import json
 import datetime
 
 
 class client:
-    def __init__(self):
+    def __init__(self, username, ip_addr):
         #self.server_ip = server_ip
         #self.server_port = server_port
-        #self.username = username
-        #self.ip_addr = ip_addr
+        self.username = username
+        self.ip_addr = ip_addr
         pass
 
     def tcp_connect(self, server_ip, server_port):
         # establish TCP connection with the server
         self.server_ip = server_ip
         self.server_port = server_port
-
         client_socket = socket(AF_INET, SOCK_STREAM) 
         client_socket.connect((server_ip,server_port)) # establish the TCP Connection 
-        #self.client_socket = client_socket
+       
 
     def send_command(self, command, body):
         # send command message to the server
@@ -30,7 +28,7 @@ class client:
             "seqNo": 2001,
             "senderId": self.username,
             "timestamp": datetime.datetime.now().isoformat(),
-            "bodyLength": len(json.dumps(body))
+            "bodyLength": len(body)
              }
         
         message = {
@@ -41,13 +39,43 @@ class client:
     
     def send_data(self, command, body):
         # send data message to the server
-        pass
+        header  ={
+            "msgType": "DATA",
+            "command": command,
+            "version": "MMMP/1.0",
+            "seqNo": 2002,
+            "senderId": self.username,
+            "timestamp": datetime.datetime.now().isoformat(),
+            "bodyLength": len(body)
+            }
+        message = {
+            "header": header,
+            "body": body
+        }
+        return message  
 
     def receive_message(self, message):
         # receive message from the server and process it
         #Still need to disect the message to get the request
-        pass
-
+        message_dict = message
+        header = message_dict["header"]
+        body = message_dict["body"]
+        
+        if header["msgType"] == 'ACK':
+            # processes the acknowledgement 
+            pass
+        
+        elif header["msgType"] == "ERROR":
+            print("Received ERROR message:", body)
+            pass
+        
+        elif header["msgType"] == "CONNECT GRANT":
+            # provides the target client's IP address and port number
+            pass
+        
+        elif header["msgType"] == "PING":
+            # respond with a PONG message to allow availability check
+            pass
 
 """
 
@@ -92,7 +120,7 @@ class client:
     "command": "GTEXT_MESSAGE",
     "version": "MMMP/1.0",
     "seqNo": 2002,
-    "senderId": "client_23"
+    "senderId": self.username,
     "timestamp": "2026-02-27T10:02:15Z",
     "bodyLength": 32
   },
