9e97f83a0eb57e2f359464cb7505958e2076fc68
Phiwumusa Ngidi <ngidiwell@icloud.com>
2026-03-02 14:57:10 +0200

    skeleton update

--- Application_server.py
+++ Application_server.py
@@ -1,7 +1,44 @@
-# Application server
+# Application Server
+from socket import *
+from User_Management import User_management as usm
+from User_Management import Database_manager as database
 
 
 class Server:
     def __init__(self):
-        import socket as s
-        pass
+        self.user_man = usm()
+        server_port = 12000
+        
+        self.server_socket = socket(AF_INET, SOCK_STREAM) # SOCK_STREAM: TCP 
+        self.server_socket.bind(('', server_port)) # Binds or locks the port number to be 'server_port' instead of the assigning it to the OS
+        self.server_socket.listen(50) # waits for a client connection
+        self.connection_socket, self.addr = self.server_socket.accept() #accepts clients establishing connections
+        
+
+    # Function that processes requests and returns responses to the client application
+    """
+      LOGIN > Initiates a client session.
+    • LOGOUT > Terminates a client session.
+    • REGISTER > Registers a new client identity.
+    • JOIN > Adds a client to a group communication room.
+    • EXIT > Removes a client from a group communication room.
+    • CONNECT_REQUEST > client asks the server for another clients IP/port
+    
+    """
+
+    def response(self, request):
+
+        requests = {1:"LOGIN", 2:"LOGOUT", 3:"REGISTER", 4:"JOIN", 5:"EXIT", 6:"CONNECT_REQUEST"}
+
+        # user functions
+        login = lambda x, y:self.user_man.login(x, y)
+        logout = lambda user:self.user_man.logout(user)
+        register = lambda x, y: self.user_man.register(x, y)
+        logout = lambda user:self.user_man.logout(user) 
+        join = lambda user:self.user_man.join(user)
+        exit = lambda user:self.user_man.exit(user)
+        connect_request = lambda user1, user2 :self.user_man.connect_client(user1, user2)
+
+
+    def terminate(self):
+        self.connection_socket.close()
--- User_Management.py
+++ User_Management.py
@@ -5,16 +5,33 @@ import pandas as pd
 
 class User_management:
     def __init__(self):
-        pass
+        self.online_users = [] # list of USER OBJECTS (class user [from client app], user:={name: ip address})
 
-
+    def update_online_users(self, user):
+        self.online_users.append(user)
 
     def login(self, user, password):
         
         pass
 
+    def logout(self, user):
+        
+        pass
+
     def create(self, user, password):
         # Create a client-to-client & group chat
+        pass
+
+    def register(self, user, password):
+        pass
+
+    def join(self, user):
+        pass
+
+    def exit(self, user):
+        pass
+
+    def connect_client(self, user1, user2):
         pass
 
 class Database_manager:
