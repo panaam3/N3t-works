28b866876275f5f2fb4197436dfb5d4e665c8bf7
Phiwumusa Ngidi <ngidiwell@icloud.com>
2026-03-02 15:18:38 +0200

    added class user

--- Client_Application.py
+++ Client_Application.py
@@ -5,6 +5,16 @@ class client:
         pass
 
 
+
+class User:
+    def __init__(self, name, ip_adrr):
+        self.name = name
+        self.ip = ip_adrr
+
+    def get_user(self):
+        return (self.name, self.ip)
+    
+
 """
 
 1. command message: 
